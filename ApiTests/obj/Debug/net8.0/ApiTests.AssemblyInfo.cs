611cac49233cf874683a3971e314716ad053b454
PardeepOhri <pardeepohri1@gmail.com>
2025-09-18 07:01:06 +0530

    Commit

--- ApiTests/obj/Debug/net8.0/ApiTests.AssemblyInfo.cs
+++ ApiTests/obj/Debug/net8.0/ApiTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5596d542f6c414cf6468402287f7eac903718874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59a7aca8c871365122f38dae5544da95ef6f1a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
